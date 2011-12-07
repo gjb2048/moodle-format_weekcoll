dda6905745ce5b9c335ebb1517f9fa92fd12b9e2
gjb2048 <gjbarnard@gmail.com>
2011-12-07 23:45:39 +0000

    Completed Brazilian translation.

--- lang/pt_br_utf8/format_weekcoll.php
+++ lang/pt_br_utf8/format_weekcoll.php
@@ -7,20 +7,21 @@
  * @copyright  2009-2011 @ G J Barnard in respect to modifications of standard weeks format.
  * @link       http://docs.moodle.org/en/Collapsed_Weeks_course_format
  * @license    http://creativecommons.org/licenses/by-sa/3.0/ Creative Commons Attribution-ShareAlike 3.0 Unported (CC BY-SA 3.0)
+ * @comment    A big thank you to Tarcísio Nunes (http://moodle.org/user/profile.php?id=1149633) for the translation.
  */
 
-// Brazillian Translation of Collapsed Weeks Course Format
-// Tradução brasileira das Semanas Fechado formato
+// Brazilian Translation of Collapsed Weeks Course Format
+// Tradução brasileira de formato do curso Semanas Recolhidas
 
 // Used by the Moodle Core for identifing the format and displaying in the list of formats for a course in its settings.
 // Usado pelo do Moodle para identificar os formatos e exibir na lista de formatos para um curso em suas configurações.
-$string['nameweekcoll']='Semanas Fechado';
-$string['formatweekcoll']='Semanas Fechado';
-$string['pluginname'] = 'Semanas Fechado';
+$string['nameweekcoll']='Semanas Recolhidas';
+$string['formatweekcoll']='Semanas Recolhidas';
+$string['pluginname'] = 'Semanas Recolhidas';
 
 // Used in format.php
 // Usado em format.php
-$string['weekcolltoggle']='Toggle';
+$string['weekcolltoggle']='Alternar';
 $string['weekcolltogglewidth']='width: 44px;';
 
 // Toggle all - Moodle Tracker CONTRIB-3190
