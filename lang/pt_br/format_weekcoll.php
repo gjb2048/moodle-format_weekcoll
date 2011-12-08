84c1b6494cfa96cabab74cfbbc3d4028ed491611
gjb2048 <gjbarnard@gmail.com>
2011-12-08 21:04:38 +0000

    CONTRIB-2497

--- lang/pt_br/format_weekcoll.php
+++ lang/pt_br/format_weekcoll.php
@@ -32,6 +32,5 @@ $string['weekcollclosed']='Feche';
 // Moodle 2.0 Enhancement - Moodle Tracker MDL-15252, MDL-21693 & MDL-22056 - http://docs.moodle.org/en/Development:Languages
 // Moodle 2.0 Realce - Moodle Tracker MDL-15252, MDL-21693 & MDL-22056 - http://docs.moodle.org/en/Development:Languages
 $string['sectionname'] = 'Semana';
-$string['pluginname'] = 'Semanas Recolhidas';
 $string['section0name'] = 'Geral';
 ?>
