ed2e9b667beb3415f9ca136af338356bc787e21b
gjb2048 <gjbarnard@gmail.com>
2012-01-09 21:14:12 +0000

    Licence fix.

--- lib.php
+++ lib.php
@@ -1,34 +1,23 @@
-<?php // $Id: lib.php,v 1.8 2011/10/06 15:45:14 gb2048 Exp $
+<?php
 /**
- * Collapsed Weeks Information
- *
- * A week based format that solves the issue of the 'Scroll of Death' when a course has many weeks. All
- * weeks have a toggle that displays that week. The current week is displayed by default. One or more
- * weeks can be displayed at any given time. Toggles are persistent on a per browser session per course
- * basis but can be made to perist longer by a small code change. Full installation instructions, code
- * adaptions and credits are included in the 'Readme.txt' file.
- *
- * @package    course/format
- * @subpackage weekcoll
- * @version    See the value of '$plugin->version' in version.php.
- * @copyright  &copy; 2009-onwards G J Barnard in respect to modifications of standard weeks format.
- * @author     G J Barnard - gjbarnard at gmail dot com and {@link http://moodle.org/user/profile.php?id=442195}
- * @link       http://docs.moodle.org/en/Collapsed_Weeks_course_format
- * @license    http://www.gnu.org/copyleft/gpl.html GNU Public License
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+   This file contains general functions for the course format Collapsed Weeks
+   Thanks to Sam Hemelryk who modified the Moodle core code for 2.0, and
+   I have copied and modified under the terms of the following license:
+   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ 
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   any later version.
 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
 
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see http://www.gnu.org/licenses/.
+*/
 
 /**
  * Indicates this format uses sections.
