81a028dfcdaea618e2df489b0bd13db5c18d0ac9
gjb2048 <gjbarnard@gmail.com>
2012-01-09 22:27:36 +0000

    Fix licence.

--- lang/pt/format_weekcoll.php
+++ lang/pt/format_weekcoll.php
@@ -1,12 +1,33 @@
-﻿<?php // $Id: format_weekcoll.php,v 1.4 2011/10/06 15:45:14 gb2048 Exp $
+﻿<?php
 /**
  * Collapsed Weeks Information
  *
+ * A week based format that solves the issue of the 'Scroll of Death' when a course has many weeks. All
+ * weeks have a toggle that displays that week. The current week is displayed by default. One or more
+ * weeks can be displayed at any given time. Toggles are persistent on a per browser session per course
+ * basis but can be made to perist longer by a small code change. Full installation instructions, code
+ * adaptions and credits are included in the 'Readme.txt' file.
+ *
  * @package    course/format
  * @subpackage weekcoll
- * @copyright  2009-2011 @ G J Barnard in respect to modifications of standard weeks format.
+ * @version    See the value of '$plugin->version' in version.php.
+ * @copyright  &copy; 2009-onwards G J Barnard in respect to modifications of standard weeks format.
+ * @author     G J Barnard - gjbarnard at gmail dot com and {@link http://moodle.org/user/profile.php?id=442195}
  * @link       http://docs.moodle.org/en/Collapsed_Weeks_course_format
- * @license    http://creativecommons.org/licenses/by-sa/3.0/ Creative Commons Attribution-ShareAlike 3.0 Unported (CC BY-SA 3.0)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU Public License
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 // Portuguese Translation of Collapsed Weeks Course Format
